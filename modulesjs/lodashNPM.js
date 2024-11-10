3603edd586af1d4b3b0ca847689049eda65748a8
faizal04 <100855720+faizal04@users.noreply.github.com>
2024-11-10 22:59:36 +0530

    lodash-es

--- modulesjs/lodashNPM.js
+++ modulesjs/lodashNPM.js
@@ -1,6 +1,9 @@
 // Importing cloneDeep from lodash-es
 import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+// to run this file you have to download lodash-es 
 //npm i lodash-es
+// you can also use command npm i which will atomatically download lodash-es
 
 // Defining a nested object
 const nestedObject = {
@@ -30,7 +33,11 @@ console.log(deepclone);          // deepclone remains unaffected by the change
 console.log(nestedObjectClone); // nestedObjectClone is affected by the change
 
 
-
 // The Object.assign() method creates a shallow copy of nestedObject.This means it only copies the top - level properties of the object.When you modify nestedObject.bookscart[0].namee, it also changes the nestedObjectClone.bookscart[0].namee, because they are both referencing the same array.
 
 // The cloneDeep() method creates a completely independent copy of nestedObject, including all nested objects and arrays. This is a deep clone, so changes to nestedObject will not affect deepclone
+
+if (module.hot) {
+    module.hot.accept();
+
+}
